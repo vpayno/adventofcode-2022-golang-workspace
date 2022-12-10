c536b25de52a69abbed439fe5d62787598828196
Victor Payno <vpayno@users.noreply.github.com>
2022-12-10 10:09:11 -0800

    challenge: add day01 tests, part 2

--- cmd/day01/aoc-day01_test.go
+++ cmd/day01/aoc-day01_test.go
@@ -4,7 +4,10 @@ import (
 	"bytes"
 	"io"
 	"os"
+	"path/filepath"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 // This is the main test function. This is the gatekeeper of all the tests in the main package.
@@ -31,6 +34,52 @@ func TestMain_app(t *testing.T) {
 	}()
 
 	want := "67633\n199628\n"
+
+	// Run the function who's output we want to capture.
+	main()
+
+	// Stop capturing stdout.
+	writer.Close()
+
+	var buf bytes.Buffer
+	_, err = io.Copy(&buf, testStdout)
+	if err != nil {
+		t.Error(err)
+	}
+	got := buf.String()
+	if got != want {
+		t.Errorf("main(); want %q, got %q", want, got)
+	}
+}
+
+// Test Run() failures in main()
+func TestMain_runError(t *testing.T) {
+	os.Args = []string{}
+
+	testStdout, writer, err := os.Pipe()
+	if err != nil {
+		t.Errorf("os.Pipe() err %v; want %v", err, nil)
+	}
+
+	osStdout := os.Stdout // keep backup of the real stdout
+	os.Stdout = writer
+
+	defer func() {
+		// Undo what we changed when this test is done.
+		os.Stdout = osStdout
+	}()
+
+	fileRoot, err := os.Getwd()
+	assert.Nil(t, err, "failed to get CWD")
+
+	fileRoot = filepath.Clean(fileRoot + "/../../")
+
+	want := "Encountered error while running app.Run()\n\n"
+	want += "open " + fileRoot
+	want += "/data/day00/day00-input.txt: no such file or directory\n"
+
+	// This challenge doesn't exist.
+	challengeName = "day00"
 
 	// Run the function who's output we want to capture.
 	main()
--- internal/day01/app_test.go
+++ internal/day01/app_test.go
@@ -23,11 +23,26 @@ func TestMain(m *testing.M) {
 	os.Exit(exitCode)
 }
 
+func TestRun(t *testing.T) {
+	conf := Setup("day01")
+
+	err := Run(conf)
+	assert.Nil(t, err, "Run() returned an error")
+}
+
+func TestRun_fileError(t *testing.T) {
+	// this tests fails because it can't find the file
+	conf := Setup("day00")
+
+	err := Run(conf)
+	assert.NotNil(t, err, "Run() didn't fail")
+}
+
 func TestGetFile(t *testing.T) {
 	fileName := "data/day01/day01-input.txt"
 
 	fileRoot, err := os.Getwd()
-	assert.Nil(t, err)
+	assert.Nil(t, err, "failed to get CWD")
 
 	fileRoot = filepath.Clean(fileRoot + "/../../")
 	testFile := fileRoot + "/" + fileName
@@ -36,44 +51,44 @@ func TestGetFile(t *testing.T) {
 
 	gotFile, gotErr := getFile(fileName)
 
-	assert.Nil(t, wantErr)
-	assert.Nil(t, gotErr)
+	assert.Nil(t, wantErr, "failed to open wanted file")
+	assert.Nil(t, gotErr, "failed to open got file")
 
 	assert.Equal(t, wantFile.Name(), gotFile.Name(), "file names don't match")
 }
 
 func TestGetFile_NoCW(t *testing.T) {
 	wd, err := os.Getwd()
-	assert.Nil(t, err)
+	assert.Nil(t, err, "failed to get CWD, should always work here")
 
 	tmpDir, err := os.MkdirTemp("/tmp", "TestGetFile_NoCW")
-	assert.Nil(t, err)
+	assert.Nil(t, err, "failed to create temporary directory")
 
 	err = os.Chdir(tmpDir)
-	assert.Nil(t, err)
+	assert.Nil(t, err, "failed to cd to temporary directory")
 
 	err = os.Remove(tmpDir)
-	assert.Nil(t, err)
+	assert.Nil(t, err, "failed to remove temporary directory")
 
 	_, err = getFile("data/day01/day01-input.txt")
-	assert.NotNil(t, err)
+	assert.NotNil(t, err, "getFile() should have failed here")
 
 	err = os.Chdir(wd)
-	assert.Nil(t, err)
+	assert.Nil(t, err, "failed to return to the orginal working directory")
 }
 
 func TestGetScanner(t *testing.T) {
 	fileName := "data/day01/day01-input.txt"
 
 	wantScanner, wantErr := getFile(fileName)
+	assert.Nil(t, wantErr, "failed to get wantScanner")
+
 	want := bufio.NewScanner(wantScanner)
 
-	assert.Nil(t, wantErr)
-
 	gotScanner, gotErr := getFile(fileName)
-	got := getScanner(gotScanner)
+	assert.Nil(t, gotErr, "failed to get gotScanner")
 
-	assert.Nil(t, gotErr)
+	got := getScanner(gotScanner)
 
 	for {
 		if !want.Scan() {
@@ -91,6 +106,29 @@ func TestGetScanner(t *testing.T) {
 }
 
 func TestLoadData(t *testing.T) {
+	want := map[string]int{
+		"elf1": 51_844,
+		"elf2": 44_141,
+		"elf3": 28_132,
+		"elf4": 49_298,
+		"elf5": 26_247,
+	}
+
+	fileName := "data/day01/day01-input.txt"
+
+	file, err := getFile(fileName)
+	assert.Nil(t, err, "failed to get file")
+
+	scanner := getScanner(file)
+
+	got, err := loadData(scanner)
+	assert.Nil(t, err, "failed to load file")
+
+	for key, wantValue := range want {
+		gotValue, gotFound := got[key]
+		assert.True(t, gotFound, "wanted key, "+key+", not found in dictionary")
+		assert.Equal(t, wantValue, gotValue, "want/got values don't match for key ["+key+"]")
+	}
 }
 
 func TestGetMaxCalories(t *testing.T) {
@@ -98,6 +136,8 @@ func TestGetMaxCalories(t *testing.T) {
 		"elf1": 2_000,
 		"elf2": 8_000,
 		"elf3": 5_000,
+		"elf4": 1_000,
+		"elf5": 3_000,
 	}
 
 	want := 8_000
